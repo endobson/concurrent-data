2daf921f0c6b5760553e52a154517767672a2fbc
Eric Dobson <eric.n.dobson@gmail.com>
2012-12-31 10:03:53 -0800

    Made space safe version fast.

--- data/concurrent-nonblocking-queue.rkt
+++ data/concurrent-nonblocking-queue.rkt
@@ -17,43 +17,42 @@
 (struct queue (head tail)
         #:property prop:sequence
         (λ (q) (queue->sequence q)))
-(struct node ((value #:mutable) next))
+(struct node ((value #:mutable) next-box))
 
 (define (make-queue)
-  (define new-node (node #f (box #f)))
-  (queue (box new-node) (box new-node)))
+  (define the-box (box #f))
+  (queue (box the-box) (box the-box)))
 
 
 (define (enqueue! q v)
-  (define new-node (node v (box #f)))
+  (define new-box (box #f))
+  (define new-node (node v new-box))
   (let loop ()
     (let* ((tail-box (queue-tail q))
-           (tail (unbox tail-box))
-           (next-box (node-next tail))
+           (next-box (unbox tail-box))
            (next (unbox next-box)))
       (if next 
-          (begin (box-cas! tail-box tail next) (loop))
-          (unless (box-cas! next-box next new-node)
-            (loop))))))
+          (begin (box-cas! tail-box next-box (node-next-box next)) (loop))
+          (if (box-cas! next-box next new-node)
+              (box-cas! tail-box next-box new-box)
+              (loop))))))
 
 ;;TODO figure out how to make this space safe and fast at the same time
 (define (dequeue! q (empty-fn (lambda () #f)))
   (let loop ()
     (let* ((head-box (queue-head q))
-           (head (unbox head-box))
+           (head-node-box (unbox head-box))
            (tail-box (queue-tail q))
-           (tail (unbox tail-box))
-           (next (unbox (node-next head))))
-      (if (eq? head tail)
+           (tail-node-box (unbox tail-box))
+           (next (unbox head-node-box)))
+      (if (eq? head-node-box tail-node-box)
           (if next
-              (begin (box-cas! tail-box tail next) (loop))
+              (begin (box-cas! tail-box tail-node-box (node-next-box next)) (loop))
               (empty-fn))
-          (if (box-cas! head-box head next)
-              (begin0
-                (node-value next)
-                (set-node-value! next #f))
+          (if (box-cas! head-box head-node-box (node-next-box next))
+              (node-value next)
               (loop))))))
-
+#;
 (define (fast-dequeue! q (empty-fn (lambda () #f)))
   (let loop ()
     (let* ((head-box (queue-head q))
@@ -72,13 +71,13 @@
 
 (define (queue-length queue)
   (let loop ((count 0) (node (unbox (queue-head queue))))
-    (let ((next-node (unbox (node-next node))))
+    (let ((next-node (unbox (node-next-box node))))
       (if next-node
           (loop (add1 count) next-node)
           count))))
 
 (define (queue-empty? queue)
-  (not (unbox (node-next (unbox (queue-head queue))))))
+  (not (unbox (node-next-box (unbox (queue-head queue))))))
 
 (define (queue->sequence q)
   (define stop (gensym 'stop))
@@ -89,7 +88,7 @@
 (module+ comparison
   (require (prefix-in r: data/queue))
 
-  (define N 4000000)
+  (define N 6000000)
   (let ((q (r:make-queue)))
     (time
       (for ((i (in-range N)))
@@ -97,7 +96,7 @@
     (time
       (for ((i (in-range N)))
         (r:dequeue! q))))
-
+#; #;
   (collect-garbage) 
   (let ((q (make-queue)))
     (time
@@ -110,6 +109,7 @@
             (fast-dequeue! q))))))
 
   (collect-garbage) 
+  (collect-garbage)
   (let ((q (make-queue)))
     (time
       (for ((i (in-range N)))
